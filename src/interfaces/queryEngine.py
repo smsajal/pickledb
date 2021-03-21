7015e9fbc2bfbbf431622355980a5d24caced128
Avimita-amc8313 <amc8313@psu.edu>
2021-03-20 21:53:02 -0400

    bulk insert from JSON file

--- src/interfaces/queryEngine.py
+++ src/interfaces/queryEngine.py
@@ -17,6 +17,12 @@ def bulkInsertTable(tableObj,bulkData):
     storageInterface.bulkInsert(tableObj,bulkDataList)
     print("Bulk Insert Completed Successfully!")
 
+def bulkInsertTableJSON(tableObj,bulkDataJSONpath):
+    file = open(bulkDataJSONpath)
+    bulkDataList = json.load(file)
+    storageInterface.bulkInsert(tableObj, bulkDataList)
+    print("Bulk Insert Completed Successfully!")
+
 def tableInsert(tableObj,data):
     dataList = json.loads(data)
     storageInterface.insert(tableObj,dataList)
@@ -34,11 +40,12 @@ def interface():
     print("1. Create DataBase")
     print("2. Create Table")
     print("3. Bulk Insert Into Table")
-    print("4. Single Insert Into Table")
-    print("5. Select Columns from Table")
-    print("6. Quit")
+    print("4. Bulk Insert Into Table from JSON file")
+    print("5. Single Insert Into Table")
+    print("6. Select Columns from Table")
+    print("7. Quit")
     print(30 * '-')
-    choice = int(input("Enter your choice [1-6]: "))
+    choice = int(input("Enter your choice [1-7]: "))
     return choice
 
 def interfaceCalls(choice):
@@ -56,28 +63,35 @@ def interfaceCalls(choice):
         TableObj = dbTable( tableName = tableName, dbName = dbName , primaryKey = primaryKey)
         bulkData = input("Enter the Bulk Data is the form of a JSON array: ")
         bulkInsertTable(TableObj,bulkData)
-    elif (choice == 4):                    # Single Tuple Insert in table
+    elif (choice == 4):                 # Bulk Insert in Table from JSON file
+        dbName = input("Enter the name of the DataBase to be used: ")
+        tableName = input("Enter the name of the Table to be used: ")
+        primaryKey = input("Enter the Primary Key Field: ")
+        TableObj = dbTable(tableName=tableName, dbName=dbName, primaryKey=primaryKey)
+        bulkDataPath = input("Enter the path to the JSON file: ")
+        bulkInsertTableJSON(TableObj,bulkDataPath)
+    elif (choice == 5):                    # Single Tuple Insert in table
         dbName = input("Enter the name of the DataBase to be used: ")
         tableName = input("Enter the name of the Table to be used: ")
         primaryKey = input("Enter the Primary Key Field: ")
         TableObj = dbTable(tableName=tableName, dbName=dbName, primaryKey=primaryKey)
         singleData = input("Enter the Single Data Row is the form of a JSON object: ")
         tableInsert(TableObj,singleData)
-    elif (choice == 5):                    # Simple Select from Table
+    elif (choice == 6):                    # Simple Select from Table
         dbName = input("Enter the name of the DataBase to be used: ")
         tableName = input("Enter the name of the Table to be used: ")
         primaryKey = input("Enter the Primary Key Field: ")
         TableObj = dbTable(tableName=tableName, dbName=dbName, primaryKey=primaryKey)
         columns = input("Enter the columns separated by a ',': ")
         simpleSelect(TableObj,columns)
-    elif (choice == 6):                   #Quit
+    elif (choice == 7):                   #Quit
         print("QUIT")
     else:                                 #Invalid Choice
         print("Invalid Choice.")
 
 def main():
         choice = 0
-        while (choice < 6):
+        while (choice < 7):
             choice = interface()
             interfaceCalls(choice)
 
